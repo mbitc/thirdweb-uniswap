5c4c16f7b2bf0051fb07c0a8294caa297e0fa135
Marius Ratnikas <marius.ratnikas@gmail.com>
2024-05-22 12:10:11 +0300

    add example env and change anvil connection

--- src/lib/anvil.ts
+++ src/lib/anvil.ts
@@ -1,13 +1,13 @@
-import { defineChain } from "thirdweb"
+import { defineChain } from 'thirdweb';
 
 export default defineChain({
-    id: 31337,
-    name: "Anvil",
-    rpc: "http://localhost:8545",
-    testnet: true,
-    nativeCurrency: {
-        name: "Anvil Ether",
-        symbol: "ETH",
-        decimals: 18,
-    },
+  id: 31337,
+  name: 'Anvil',
+  rpc: 'https://rpc.ankr.com/eth',
+  testnet: true,
+  nativeCurrency: {
+    name: 'Anvil Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
 });
