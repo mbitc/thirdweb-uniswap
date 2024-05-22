6fb2b6533256b329e641c85cf467ba8bfd7827a2
Marius Ratnikas <marius.ratnikas@gmail.com>
2024-05-22 11:55:11 +0300

    connection update

--- src/lib/thirdweb-client.ts
+++ src/lib/thirdweb-client.ts
@@ -1,6 +1,12 @@
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient } from 'thirdweb';
 
-export default createThirdwebClient({
-    clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
-});
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID!;
+const secretKey = process.env.THIRDWEB_SECRET_KEY!;
 
+export default createThirdwebClient(
+  secretKey
+    ? { secretKey }
+    : {
+        clientId,
+      }
+);
